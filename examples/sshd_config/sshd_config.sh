bbd48f8443a705b2a18eb1e578cbc6b009287534
Radek Krejci <rkrejci@cesnet.cz>
2015-05-04 15:37:31 +0200

    Examples: fix pyang command to create sshd_config.xml
    Wrong pyang options used.

--- examples/sshd_config/sshd_config.sh
+++ examples/sshd_config/sshd_config.sh
@@ -1,2 +1,2 @@
-pyang -f sample-skeleton --sample-skeleton-defaults sshd_config.yang > sshd_config.xml
+pyang -f sample-xml-skeleton --sample-xml-skeleton-defaults sshd_config.yang > sshd_config.xml
 xsltproc sshd_config.xsl sshd_config.xml > sshd_config
